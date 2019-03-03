9aa701e8707480b093437508b6f390403b76a68c
HappyArroW <stifmeister0700@gmail.com>
2019-03-03 19:28:46 +0100

    jeszcze raz od nowa ;D

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private Rigidbody2D playerRigidbody;
+
+	[SerializeField]
+	private float movementSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		float horizontal = Input.GetAxis("Horizontal");
+		HandleMovement(horizontal);
+    }
+
+	private void HandleMovement(float horizontal)
+	{
+		playerRigidbody.velocity = new Vector2(horizontal * movementSpeed, playerRigidbody.velocity.y);
+	}
+}
